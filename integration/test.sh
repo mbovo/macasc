e725c902d3a471599469ad67cca6a54a1a83d26a
Manuel Bovo <manuel.bovo@gmail.com>
2020-08-30 14:23:43 +0200

    WIP test cases

--- integration/test.sh
+++ integration/test.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 
+function checkStdOut() {
+  diff -q "$1" "$2" | grep -o differ
+  if [ $? -eq 0 ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function checkStdErr(){
+  if [ -e "$1" ]; then
+    diff -q "$1" "$2" | grep -o differ
+    if [ $? -eq 0 ]; then
+      return 0
+    else
+      return 1
+    fi
+  fi
+  return 0
+}
+
 for i in $(find . -type f -name "*.yml"); do
   outfile=$(basename $i .yml)
-  ../bin/yacasc verify $i > /tmp/out
-  diff ${outfile}.out /tmp/out
+  ../bin/yacasc verify $i > /tmp/out 2> /tmp/err
+
+  (checkStdOut "${outfile}.out" "/tmp/out")
   if [ $? -eq 0 ]; then
-    echo "$i  OK"
-  else
-    echo "$i  FAIL"
-    exit 1
+    b=$(checkStdErr "${outfile}.err" "/tmp/err")
+    if [ $? -eq 0 ]; then
+      echo "$i  OK"
+      continue
+    fi
   fi
+  echo "$i  FAIL"
+  exit 1
 done
 
